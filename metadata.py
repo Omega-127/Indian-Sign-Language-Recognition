762c0086e0fc35ca55959987a751a1914528ac1e
Sarvesh Haldikar <srhaldikar711@gmail.com>
2026-06-23 06:18:07 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,1 +1,11 @@
-from 
+from datasets import load_datasets
+import pandas as pd
+
+def main():
+    print("Loading INCLUDE dataset metadata from hugging face...")
+    dataset = load_datasets("ai4baharat/INCLUDE")
+
+    all_rows = []
+
+    for split_name in dataset.keys():
+        
