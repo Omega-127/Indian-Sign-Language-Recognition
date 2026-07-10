33bac4dd8b38f4cb9cea2b15e06ce6e9641177c9
Sarvesh Haldikar <srhaldikar711@gmail.com>
2026-07-10 12:26:02 +0530

    Fixed some indentation bugs and added run_interference()

--- interference.py
+++ interference.py
@@ -30,7 +30,7 @@ pose_connections = mp.tasks.vision.PoseLandmarksConnections.POSE_LANDMARKS
 hand_connections = mp.tasks.vision.HandLandmarksConnections.HAND_CONNECTIONS
 face_connections = mp.tasks.vision.FaceLandmarksConnections.FACE_LANDMARKS_CONTOURS
 
-class BiLSTMclassifier(nn.module):
+class BiLSTMclassifier(nn.Module):
     def __init__(self, input_size=1692, hidden_size=128, num_layers=1, num_classes=50, dropout=0.5):
         super().__init__()
         self.lstm == nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, bidirectional=True)
@@ -49,29 +49,48 @@ class BiLSTMclassifier(nn.module):
         out = self.classifier(out)
         return out
     
-    def extract_landmarks(pose_landmarks, face_landmarks, left_hand, right_hand):
-        def flatten(landmark_list, count, with_visiblity=False):
-            if not landmark_list:
-                return np.zeros(count * (4 if with_visiblity else 3))
-            if with_visiblity:
-                return np.array(
-                    [[lm.x, lm.y, lm.z, lm.visibility] for lm in landmark_list]).flatten()
+def extract_landmarks(pose_landmarks, face_landmarks, left_hand, right_hand):
+    def flatten(landmark_list, count, with_visiblity=False):
+        if not landmark_list:
+            return np.zeros(count * (4 if with_visiblity else 3))
+        if with_visiblity:
             return np.array(
-                    [[lm.x, lm.y, lm.z] for lm in landmark_list]).flatten()
+                [[lm.x, lm.y, lm.z, lm.visibility] for lm in landmark_list]).flatten()
+        return np.array(
+                [[lm.x, lm.y, lm.z] for lm in landmark_list]).flatten()
     
-        pose = flatten(pose_landmarks, 33, with_visiblity=True)
-        face = flatten(face_landmarks, 478)
-        left = flatten(left_hand, 21)
-        right = flatten(right_hand, 21)
+    pose = flatten(pose_landmarks, 33, with_visiblity=True)
+    face = flatten(face_landmarks, 478)
+    left = flatten(left_hand, 21)
+    right = flatten(right_hand, 21)
 
-        return np.concatenate([pose, face, left, right])
+    return np.concatenate([pose, face, left, right])
 
-    def split_hands(hand_result):
-        left_hand, right_hand = None, None
-        for landmarks, handedness in zip(hand_result.hand_landmarks, hand_result.handedness):
-            label = handedness[0].category_name
-            if label == 'Left':
-                left_hand = landmarks
-            elif label == 'Right':
-                right_hand = landmarks
-        return left_hand, right_hand
+def split_hands(hand_result):
+    left_hand, right_hand = None, None
+    for landmarks, handedness in zip(hand_result.hand_landmarks, hand_result.handedness):
+        label = handedness[0].category_name
+        if label == 'Left':
+            left_hand = landmarks
+        elif label == 'Right':
+            right_hand = landmarks
+    return left_hand, right_hand
+
+def run_interference(model, window, device):
+    sequence = np.array(window, dtype=np.float32)
+    tensor = torch.tensor(sequence).unsqueeze(0).to(device)
+    length = torch.tensor([len(window)])
+
+    with torch.no_grad():
+        output = model(tensor, length)
+        probs = torch.softmax(output, dim = 1)
+        confidence, pred_idx = torch.max(probs, dim=1)
+    return pred_idx.item(), confidence.item()
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
+
