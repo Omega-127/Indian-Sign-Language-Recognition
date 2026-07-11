0043d1c80f471abea266b35adcacb663eee8cd18
Sarvesh Haldikar <srhaldikar711@gmail.com>
2026-07-11 16:53:47 +0530

    Add frame flipping for improved image orientation in main function

--- inference.py
+++ inference.py
@@ -140,6 +140,7 @@ def main():
         ret, frame = cap.read()
         if not ret:
             break
+        frame = cv2.flip(frame, 1)
 
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
